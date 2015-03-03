e3d2e3e46061af2875d1d7d9a332e88a08e7f3ae
Dustin Smith <thedustinsmith@gmail.com>
2015-03-03 13:42:44 -0600

    fix my previous commits with json changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,14 @@
 (function (ftk) {
 
-	var projects = [
-		{
-			name: 'ajaxQ',
-			nameLight: 'Ajax',
-			nameBold: 'Q',
-			description: 'A tiny, simple jQuery plugin for sequential ajax requests',
-			link: 'http://foliotek.github.io/AjaxQ/',
-			imgNum: 3
-		},
-		{
-			name: 'DocEditable',
-			nameLight: 'Doc',
-			nameBold: 'Editable',
-			description: 'Clean WYSIWYG editing in your browser',
-			link: 'http://foliotek.github.io/DocEditable/',
-			imgNum: 6
-		},
-	];
-
 	ftk.listing = {};
 	ftk.listing.init = function () {
 		var template = $("#project-template").html();
-		var html = Mustache.render(template, {
-			projects: projects
+		$.getJSON('projects.json').done(function (projects) {
+			var html = Mustache.render(template, {
+				projects: projects
+			});
+			$(".project-listing").html(html);
 		});
-		$(".project-listing").html(html);
 	};
 
 })(ftk);
